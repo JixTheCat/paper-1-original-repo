b76bbcd7848e47ee4cce4ffbc316139426394d93
JixTheCat <bryce.polley@gmail.com>
2023-04-14 13:20:24 +0930

    fixed old models, included new graph and fixed map - these are the references to new variables as well as the appropriate R2 scores.

--- analysis.r
+++ analysis.r
@@ -4,23 +4,28 @@ df <- read.csv("df.csv")
 # We create our first model
 anova(lm(tonnes_grapes_harvested ~ data_year_id+giregion+area_harvested+water_used+scope1,data=df))
 summary(lm(tonnes_grapes_harvested ~ data_year_id+giregion+area_harvested+water_used+scope1,data=df))
-#0.9077
+# 0.9059
 
 # We add Value to the model :P
 anova(lm(value ~ data_year_id+giregion+area_harvested+water_used+scope1,data=df))
 summary(lm(value ~ data_year_id+giregion+area_harvested+water_used+scope1,data=df))
-# 0.9296
-
+# 0.9747
 
 # Lets look at ratios
-anova(lm(df$tha ~ df$area_harvested+df$scope1+df$water_used+df$data_year_id:df$giregion))
-summary(lm(df$tha ~ df$area_harvested+df$scope1+df$water_used+df$data_year_id:df$giregion))
+anova(lm(df$ha_tonnes_grapes_harvested ~ df$area_harvested*((df$scope1)+df$water_used)+df$data_year_id:df$giregion))
+summary(lm(df$ha_tonnes_grapes_harvested ~ df$area_harvested*((df$scope1)+df$water_used)+df$data_year_id:df$giregion))
+# 0.7763
 
-anova(lm(df$vtha ~ df$area_harvested+df$scope1+df$water_used+df$data_year_id:df$giregion))
-summary(lm(df$vtha ~ df$area_harvested+df$scope1+df$water_used+df$data_year_id:df$giregion))
+anova(lm(df$ha_value ~ df$area_harvested*((df$scope1)+df$water_used)+df$data_year_id:df$giregion))
+summary(lm(df$ha_value ~ df$area_harvested*((df$scope1)+df$water_used)+df$data_year_id:df$giregion))
+# 0.9637 
 
 # note that vtha~tha is not a great predictor of one another.
-summary(lm(df$vtha ~ df$tha*df$giregion*df$data_year_id))
+# but it is a significant variable!
+summary(lm(df$ha_value ~ df$ha_tonnes_grapes_harvested))
+anova(lm(df$ha_value ~ df$ha_tonnes_grapes_harvested))
+
+# note that scope1 is only good for predicting the amount of grapes not the quality.
 
 # here are some plots
 plot(df$area_harvested , df$tonnes_grapes_harvested*df$average_per_tonne, col=factor(df$giregion))
@@ -32,20 +37,20 @@ library(terra)
 v <- vect("data/Wine_Geographical_Indications_Australia_2022/Wine_Geographical_Indications_Australia_2022.shp")
 aus <- vect("data/STE_2021_AUST_SHP_GDA2020/STE_2021_AUST_GDA2020.shp")
 
-v$tha <- 0
-v$vtha <- 0
+v$ha_tonnes_grapes_harvested <- 0
+v$ha_value <- 0
 
 mean.t <- function(x) {mean(x, na.rm=TRUE)}
-aggs.tha <- aggregate(df$tha, list(df$giregion), FUN=mean.t)
-aggs.vtha <- aggregate(df$vtha, list(df$giregion), FUN=mean.t)
+aggs.ha_tonnes_grapes_harvested <- aggregate(df$ha_tonnes_grapes_harvested, list(df$giregion), FUN=mean.t)
+aggs.ha_value <- aggregate(df$ha_value, list(df$giregion), FUN=mean.t)
 
 for (region in unique(df$giregion))
 {
-v[v$GI_NAME==region , ]$tha <- aggs.tha[aggs$Group.1==region , ]$x
-v[v$GI_NAME==region , ]$vtha <- aggs.vtha[aggs$Group.1==region , ]$x
+v[v$GI_NAME==region , ]$ha_tonnes_grapes_harvested <- aggs.ha_tonnes_grapes_harvested[aggs.ha_tonnes_grapes_harvested$Group.1==region , ]$x
+v[v$GI_NAME==region , ]$ha_value <- aggs.ha_value[aggs.ha_value$Group.1==region , ]$x
 }
 
-plot(v, "tha")
+plot(v, "ha_tonnes_grapes_harvested")
 lines(aus)
 
 #    data (or map) frame
@@ -56,5 +61,29 @@ lines(aus)
 #    metadata (or map citation)
 #    border (or neatline)
 #    inset (or locator) map.
+
+spl <- lm(df$tha ~ bs(df$water_used, df=3) )
+x_lim <- range(df$tha, na.rm=TRUE)
+x_grid <- seq(x_lim[1], x_lim[2])
+preds <- predict(spl, list(x_grid))
+plot(df$tha, df$water_used)
+lines(x_grid, preds, col="blue", lty="dotted")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
